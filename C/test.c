7f23e213cf54748f073bdd66b1178a523436d216
Mun Jun HO <jhmun0607@gmail.com>
2024-07-31 08:08:23 +0000

    feat: c언어 vim을 통해서연습

---
+++ C/test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+	printf("\\   \/\\");
+	printf("\n");
+	printf(" )  ( ')");
+	printf("\n");
+	printf("(  \/  )");
+	printf("\n");
+	printf(" \\(__)\|");
+
+
+	return 0;
+
+}
