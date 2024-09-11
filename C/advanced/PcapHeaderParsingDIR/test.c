de410371455581375068500faa1f4751828e9314
Mun Jun HO <jhmun0607@gmail.com>
2024-09-11 15:46:01 +0900

    fix: 디렉터리 이름변경

---
+++ C/advanced/PcapHeaderParsingDIR/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pcap.h>
+
+int main(void)
+{
+
+    char* dev;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    dev = pcap_lookupdev(errbuf);
+    if(dev == NULL)
+    {
+        printf("장치를 자동으로 찾을 수 없습니다.\n");
+    }
+    printf("장치 이름 : %s \n", dev);
+    return 0;
+}
